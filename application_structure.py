b628d1f71750939b36bba9692cc5e586a17c70ca
Alice Villar <alsaldanhavillar@gmail.com>
2021-07-02 13:05:48 -0300

    reorganizing images

---
+++ application_structure.py
@@ -0,0 +1,15 @@
+import os
+
+# Directory tree structure in python
+
+
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+list_files(os.getcwd())
--- controllers/limiters_controller.py
+++ controllers/limiters_controller.py
@@ -1,3 +1,6 @@
+from flask import request
+
+
 # Protecting against DDOs attacks
 
 from flask import Flask
@@ -15,4 +18,17 @@ ip_limiter = Limiter(
     default_limits=["50 per day", "50 per hour"]
 )
 
+# função para auxiliar a biblioteca de limitação. Essa função vai fazer uma contagem do numero de requests por chave
+def get_key():
+    try:
+        api_key = request.headers['X-API-KEY']
+        return api_key
 
+    except: # se não foi forcencida uma api key: #avisar no terminal um det erro.
+        raise Exception('Request done with no API Key. Limiting could no be done')
+
+key_usage_limiter = Limiter(
+    APP,
+    key_func=get_key, # counting the use. This functions returns the IP, so it can recognize when the same IP uses the API more than x times
+    default_limits=["50 per day", "50 per hour"]
+)
